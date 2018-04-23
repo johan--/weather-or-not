898834f69adaf723c1567d16826506b5f3725990
Andrew Moore <admoore14@gmail.com>
2018-04-23 01:33:15 -0500

    generate and upload report to codecov

--- app/src/androidTest/java/com/andrewm/weatherornot/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/andrewm/weatherornot/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.andrewm.weatherornot", appContext.packageName)
+        assertEquals("com.andrewm.weatherornot.dev", appContext.packageName)
     }
 }
